8e5478b628a934a0fbf180bfaac8bfa44a999cad
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-06-22 13:48:13 +0900

    feat :: 메인 페이지 Router 연결

---
+++ src/components/main/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./style";
+
+const Main = () => {
+    return (
+        <S.MainPage>
+            
+        </S.MainPage>
+    )
+}
+
+export default Main;
--- src/components/router/router.jsx
+++ src/components/router/router.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-import Login from "src/components/auth/login/Login";
+import Login from "src/components/auth/login/login";
 import SignUp from "src/components/auth/signUp/signUp";
+import MainPage from "src/pages/main/page";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="login" element={<Login />} />
       <Route path="signup" element={<SignUp />} />
+      <Route path="/" element={<MainPage />} />
     </Routes>
   );
 };

8caa77424e8cbd19960eb5dc3ba490148a47d006
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-06-24 10:31:13 +0900

    feat :: 메모 불러오기 서버 통신

--- src/components/memo/memo.tsx
+++ src/components/memo/memo.tsx
@@ -1,22 +1,33 @@
 import React from "react";
 import * as S from "./style";
+import useMemo from "src/hooks/memo/useMemo";
+import dayjs from "dayjs";
+import "dayjs/locale/ko";
 
 const Memo = () => {
+  const { memoData } = useMemo();
+
   return (
     <S.MemoPage>
       <S.MemoContainer>
         <S.DateWrap>
-          <S.CreateDate>생성일자: 2024.06.22</S.CreateDate>
-          <S.ModifyDate>수정일자: 2024.06.22</S.ModifyDate>
+          <S.CreateDate>
+            생성일자:{" "}
+            {dayjs(memoData.createdDateTime).locale("ko").format("YYYY.MM.DD")}
+          </S.CreateDate>
+          <S.ModifyDate>
+            수정일자:{" "}
+            {dayjs(memoData.modifiedDateTime).locale("ko").format("YYYY.MM.DD")}
+          </S.ModifyDate>
         </S.DateWrap>
         <S.TitleWrap>
           <S.TitleBox>
-            <S.Title>메모 제목</S.Title>
-            <S.Writer>작성자: Park Sihyun</S.Writer>
+            <S.Title>{memoData.title}</S.Title>
+            <S.Writer>작성자: {memoData.author}</S.Writer>
           </S.TitleBox>
         </S.TitleWrap>
         <S.ContentWrap>
-          <S.Content>메모 내용입니다.</S.Content>
+          <S.Content>{memoData.content}</S.Content>
         </S.ContentWrap>
       </S.MemoContainer>
     </S.MemoPage>
